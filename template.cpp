229b4b42fc1ac30b408281cdfa49041dfcd66cd6
Raj Bohra <rajbohra@Rajs-MacBook-Pro.local>
2019-08-29 10:50:57 +0530

    add template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <climits>
+#include <cmath>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    cout << "Hello world" << endl;
+}
